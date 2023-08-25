2719eb0958a050a01c926ffa23f6b988d7d72565
Romain Richards <96961208+RomainRichards@users.noreply.github.com>
2023-08-25 00:39:36 -0400

    Generating angular application code.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { PhonePipe } from './pipes/phone.pipe';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    PhonePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
