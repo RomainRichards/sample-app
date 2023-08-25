e394086841802ccb061c3053cc109cc430e2c8c7
Romain Richards <96961208+RomainRichards@users.noreply.github.com>
2023-08-25 00:38:24 -0400

    Generating angular application code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { authGuard } from './auth.guard';
+import { ContactListComponent } from './contact-list/contact-list.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'contacts',
+    component: ContactListComponent,
+    canActivate: [authGuard]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
